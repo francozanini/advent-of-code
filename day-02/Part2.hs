a2f949f006ec2a5704c50bb323db7437542320f9
francozanini <francozanini1995@gmail.com>
2022-05-19 18:28:05 -0300

    removed useless comment

--- day-02/Part2.hs
+++ day-02/Part2.hs
@@ -37,5 +37,4 @@ main = do
 
 testExampleCase = TestCase (assertEqual "Example case" (15, 60) (executeCommands ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]))
 
---- /                                                                               (5, 0) 0 | (5, 5) 5 | (13, 45) 5 | (13, 42) 2 | (13, 50) 10 | (15, )
 tests = TestList [TestLabel "ExampleCase" testExampleCase]
